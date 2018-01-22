b755de5a608788670b29eb863626e7da6aad772d
Denis Mihaylov <denis.mihaylov93@gmail.com>
2018-01-23 00:20:35 +0200

    Score graph

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from text_preprocessor import text_to_sentences
+from summarizer import sentence_similarity
+from summarizer import create_sentences_similarity_graph
+from page_rank import page_rank
 # from text_preprocessor import format_text
 
+SENTENCES_COUNT = 5
 
 def main():
     # format_text("article")
-    sentences = text_to_sentences("article_formatted.txt")
+    sentences = text_to_sentences("test.txt")
+    sentence_similarity(sentences[0], sentences[1])
+    graph = create_sentences_similarity_graph(sentences)
+    scores = page_rank(graph)
+    sorted_scores = sorted(enumerate(scores), key=lambda item: item[1], reverse=True)[:SENTENCES_COUNT]
+    summary = [sentences[idx] for idx, _ in sorted(sorted_scores)]
+    print("\n-----\n".join(summary))
 
 
 if __name__ == "__main__":
--- page_rank.py
+++ page_rank.py
@@ -2,19 +2,20 @@ import numpy as np
 
 # np.random.seed(0)
 # graph = np.random.rand(5, 5)
-graph = [
-    [0, 0.71518937,  0.60276338,  0.54488318,  0.4236548],
-    [0.64589411,  0,         0.891773,    0.96366276,  0.38344152],
-    [0.79172504,  0.52889492,  0,         0.92559664,  0.07103606],
-    [0.0871293,   0.0202184,   0.83261985,  0,          0.87001215],
-    [0.97861834,  0.79915856,  0.46147936,  0.78052918,  0]
-]
-
-graph[0][0] = graph[1][1] = graph[2][2] = graph[3][3] = graph[4][4] = 0
-print(graph)
+# graph = [
+#     [0, 0.71518937,  0.60276338,  0.54488318,  0.4236548],
+#     [0.64589411,  0,         0.891773,    0.96366276,  0.38344152],
+#     [0.79172504,  0.52889492,  0,         0.92559664,  0.07103606],
+#     [0.0871293,   0.0202184,   0.83261985,  0,          0.87001215],
+#     [0.97861834,  0.79915856,  0.46147936,  0.78052918,  0]
+# ]
+#
+# graph[0][0] = graph[1][1] = graph[2][2] = graph[3][3] = graph[4][4] = 0
+# print(graph)
 beta = 0.85
 
-def get_score(vertex_idx, beta, scores):
+
+def get_score(graph, vertex_idx, beta, scores):
     neighbours = [i for i in range(len(graph)) if graph[vertex_idx][i] != 0]
     weights_sum = sum(graph[vertex_idx])
     score = 0
@@ -26,10 +27,10 @@ def get_score(vertex_idx, beta, scores):
 # It is expected that A[i][i] = 0
 def page_rank(graph, eps=0.0001, beta=0.85):
     i = 0
-    scores = np.random.rand(1, 5)[0]
+    scores = np.random.rand(1, len(graph))[0]
 
     while True:
-        new_scores = [get_score(idx, beta, scores) for idx in range(len(graph))]
+        new_scores = [get_score(graph, idx, beta, scores) for idx in range(len(graph))]
         print('old scores', scores)
         print('scores: ', new_scores)
         x = [abs(scores[i] - new_scores[i]) <= eps for i in range(len(graph))]
@@ -41,6 +42,6 @@ def page_rank(graph, eps=0.0001, beta=0.85):
         i += 1
         scores = new_scores
 
-results = page_rank(graph)
+# results = page_rank(graph)
 
-print("Results:", results)
+# print("Results:", results)
