96041e86d22787aca88270f20c9f924863e9e764
Denis Mihaylov <denis.mihaylov93@gmail.com>
2018-01-22 23:09:29 +0200

    Fix create graph when sentences have total similarity with other sentences = 0

--- summarizer.py
+++ summarizer.py
@@ -12,14 +12,19 @@ STOP_WORDS = set(stopwords.words('english') + list(string.punctuation))
 def sentence_similarity(s1, s2):
     words1 = _get_words_from_sentence(s1)
     words2 = _get_words_from_sentence(s2)
+
     all_words = list(set(words1 + words2))
     word_to_index = {word: idx for idx, word in enumerate(all_words)}
+
     vector1 = _words_to_vector(words1, all_words, word_to_index)
     vector2 = _words_to_vector(words2, all_words, word_to_index)
+
     return 1 - cosine_distance(vector1, vector2)
 
 
 def create_sentences_similarity_graph(sentences):
+    print("Creating sentences similarity graph")
+
     graph = np.zeros((len(sentences), len(sentences)))
 
     for idx1, sentence1 in enumerate(sentences):
@@ -28,7 +33,9 @@ def create_sentences_similarity_graph(sentences):
                 graph[idx1][idx2] = sentence_similarity(sentence1, sentence2)
 
     for idx in range(len(sentences)):
-        graph[idx] /= graph[idx].sum()
+        row_sum = graph[idx].sum()
+        if (row_sum != 0):
+            graph[idx] /= row_sum
 
     return graph
 
